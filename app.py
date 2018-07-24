6f186805a0b10bf5bc233d4c102b45334140cc57
Sandhya G <35304513+sandhya-sago@users.noreply.github.com>
2018-07-23 21:37:54 -0500

    adding flask application

---
+++ app.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.append( os.path.abspath(os.path.join(os.path.dirname(__file__), 'dots_and_squares')))
+from flask import Flask, render_template
+from dots_and_squares import dots_and_squares
+
+
+# create instance of Flask app
+app = Flask(__name__)
+
+# create route that renders index.html template
+@app.route("/")
+@app.route("/scrape")
+def index():
+    dots_and_squares.main()
+    return "Great Game"
+    # return render_template("index.html", mars=mars_info[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
